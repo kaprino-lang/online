0ceb22a98b7d5ed2405b5d0d9a3929d5aa893f9f
Akihisa Yagi <capra314cabra@gmail.com>
2020-08-12 17:46:52 +0900

    Delete unused files

--- server/src/components/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom"
-
-import { MainContent } from "./mainContent"
-
-ReactDOM.render(
-    <MainContent compilerVersion="1.1.0" />,
-    document.getElementById("main")
-);
